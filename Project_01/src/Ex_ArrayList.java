3457ca72ba143a98ebe7b514d0beeaa26ecad42a
lenhattung <lenhattung@gmail.com>
2025-09-10 10:59:53 +0700

    u
    u

---
+++ Project_01/src/Ex_ArrayList.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tungi
+ */
+public class Ex_ArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        System.out.println(list);
+        list.remove(0);
+        System.out.println(list);
+    }
+}
